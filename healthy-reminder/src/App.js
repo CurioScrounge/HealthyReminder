7573279b153938de2aa848db047943349c873927
Fruitkid <fruinch@gmail.com>
2024-08-28 15:20:42 +0800

    working timer choosing sliders

--- healthy-reminder/src/App.js
+++ healthy-reminder/src/App.js
@@ -1,13 +1,60 @@
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 import Timer from './Timer'
 
 
 function App() {
+  const [hours, setHours] = useState(12);
+  const [minutes, setMinutes] = useState(30);
+
+  const handleHourChange = (e) => {
+    const value = parseInt(e.target.value);
+    setHours(value);
+  };
+
+  const handleMinuteChange = (e) => {
+    const value = parseInt(e.target.value);
+    setMinutes(value);
+  };
   return (
     <main>
-    <Timer />  
-    </main> 
+      <h1>Run timer until</h1>
+    <div className="time-picker">
+    <div className="time-picker__sliders">
+    <div className="time-picker__slider">
+      <input
+        type="range"
+        min="0"
+        max="23"
+        value={hours}
+        onChange={handleHourChange}
+        className="slider"
+      />
+    </div>
+    <label>Hours</label>
+    </div>
+    <div className="time-picker__output">
+      {hours.toString().padStart(2, '0')}:
+      {minutes.toString().padStart(2, '0')}
+    </div>
+    <div className="time-picker__sliders">
+    <div className="time-picker__slider">
+      <input
+        type="range"
+        min="0"
+        max="59"
+        value={minutes}
+        onChange={handleMinuteChange}
+        className="slider"
+      />
+    </div>
+    <label>Minutes</label>
+    </div>
+  </div>
+  <Timer />
+  </main>
+    
+    
   );
 }
 
